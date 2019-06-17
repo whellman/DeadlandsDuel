fdc78a1bd2152efb2deb6a012d971a17db77a0c7
Wes <ntxwes@gmail.com>
2019-06-17 17:59:48 -0500

    Fixed missing reference to node.y

--- map_objects/mapgine.py
+++ map_objects/mapgine.py
@@ -25,16 +25,16 @@ def generate_map(game_map, number_of_rooms):
     for node in bsp.pre_order():
         if node.children:
             node1, node2 = node.children
-            print('Connect the rooms:\n%s\n%s' % (node1, node2))
+            # print('Connect the rooms:\n%s\n%s' % (node1, node2))
         else:
-            print('Dig a room for %s.' % node)
-            # <BSP(x=45,y=51,width=17,height=11)level=5,leaf>
+            # print('Dig a room for %s.' % node)
+
             game_map.walkable[node.y, node.x : node.x + node.width] = False # changing x, origin y
             game_map.walkable[node.y + node.height, node.x : node.x + node.width] = False # changing x, end y
             game_map.walkable[node.y : node.y + node.height, node.x] = False # origin x, changing y
-            game_map.walkable[node.y + node.height, node.x + node.width] = False # end x, changing y
+            game_map.walkable[node.y : node.y + node.height, node.x + node.width] = False # end x, changing y
 
             game_map.transparent[node.y, node.x : node.x + node.width] = False # changing x, origin y
             game_map.transparent[node.y + node.height, node.x : node.x + node.width] = False # changing x, end y
             game_map.transparent[node.y : node.y + node.height, node.x] = False # origin x, changing y
-            game_map.transparent[node.y + node.height, node.x + node.width] = False # end x, changing y
+            game_map.transparent[node.y : node.y + node.height, node.x + node.width] = False # end x, changing y
